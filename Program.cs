c5f8b2cb4309208d4551c8a04a418d70fa2edf42
Victor Dolsan <victorddf5@gmail.com>
2022-02-04 19:44:22 -0300

    Adicionando o projeto .net mvc

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace CSharpSecaoDezoito
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            
-        }
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
