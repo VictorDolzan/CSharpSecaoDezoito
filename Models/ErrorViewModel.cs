f85c7c4a1bf6de1de48fa9e486bc2913d266268f
Victor Dolsan <victorddf5@gmail.com>
2022-02-07 19:49:39 -0300

    Inicializando com o .NET core 3.0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSharpSecaoDezoito.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Models/ViewModels/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CSharpSecaoDezoito.Models.ViewModels;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
