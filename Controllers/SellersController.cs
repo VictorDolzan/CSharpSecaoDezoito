c15eee4c3fb98302b1fe5ac31f1db8aa32b6e7a6
Victor Dolsan <victorddf5@gmail.com>
2022-02-13 22:20:28 -0300

    Criadas as services exception, criado o metodo Edit na SellersControler e o Update na SellerService

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -81,5 +81,23 @@ namespace CSharpSecaoDezoito.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
     }
 }
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using CSharpSecaoDezoito.Data;
 using CSharpSecaoDezoito.Models;
 using Microsoft.EntityFrameworkCore;
+using CSharpSecaoDezoito.Services.Exceptions;
 
 namespace CSharpSecaoDezoito.Services
 {
@@ -38,6 +39,23 @@ namespace CSharpSecaoDezoito.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found!");
+            }
+            try
+            {            
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
