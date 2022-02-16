f0084e3092df883fdfc46bca233224eaa75d62be
Victor Dolsan <victorddf5@gmail.com>
2022-02-15 21:47:05 -0300

    Preparing sales search navigation views, e modificado as imagens das paginas

---
+++ Controllers/SalesRecordsController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CSharpSecaoDezoito.Models;
+using CSharpSecaoDezoito.Models.ViewModels;
+
+namespace CSharpSecaoDezoito.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
