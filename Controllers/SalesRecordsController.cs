a2f4bb6d331898cd02df38b2d042cb94885edbfe
Victor Dolsan <victorddf5@gmail.com>
2022-02-16 21:16:39 -0300

    Implementing grouping search

--- Controllers/SalesRecordsController.cs
+++ Controllers/SalesRecordsController.cs
@@ -27,11 +27,11 @@ namespace CSharpSecaoDezoito.Controllers
         }
         public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            if(!minDate.HasValue)
+            if (!minDate.HasValue)
             {
-                minDate = new DateTime(DateTime.Now.Year,1,1);
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
             }
-            if(!maxDate.HasValue)
+            if (!maxDate.HasValue)
             {
                 maxDate = DateTime.Now;
             }
@@ -40,9 +40,20 @@ namespace CSharpSecaoDezoito.Controllers
             var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
             return View(result);
         }
-        public IActionResult GroupingSearch()
+       public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
--- Services/SalesRecordService.cs
+++ Services/SalesRecordService.cs
@@ -23,7 +23,7 @@ namespace CSharpSecaoDezoito.Services
             var result = from obj in _context.SalesRecord select obj;
             if(minDate.HasValue)
             {
-                result = result.Where(x => x.Date>= minDate.Value);
+                result = result.Where(x => x.Date >= minDate.Value);
             }
             if(maxDate.HasValue)
             {
@@ -35,5 +35,24 @@ namespace CSharpSecaoDezoito.Services
             .OrderByDescending(x => x.Date)
             .ToListAsync();
         }
+        public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.SellerSR)
+                .Include(x => x.SellerSR.DepartmentS)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.SellerSR.DepartmentS)
+                .ToListAsync();
+        }
+        
     }
 }
